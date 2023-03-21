cbede77f3ef51d4e84440d387b8d9b07edb6fd5d
rohitjethoe <rohitjethoe@proton.me>
2023-03-21 21:55:13 +0100

    Netlify adapter toegevoegd.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+  output: "server",
+  adapter: netlify()
 });
