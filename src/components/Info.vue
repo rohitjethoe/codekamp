249c943856bbc17b475b1129da19cd4e61ce8e7d
rohitjethoe <rohitjethoe@proton.me>
2023-03-21 21:53:52 +0100

    Tekst gewijzigd.

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -8,7 +8,7 @@
         <div class="content">
             <p><span>Programmeren</span> is een breed begrip. Vandaag wordt computercode schrijven toegepast op elk aspect in onze maatschappij.</p> 
             <br />
-            <p>Denk aan website, desktop en mobile development. Maar ook Database, DevOps en Security Engineering. Daarnaast heb je nog Backend applicaties die worden gemaakt met AI, Automatisering en nog veel meer.</p> 
+            <p>Denk aan website, desktop en mobile development. Maar ook Database, DevOps en Security Engineering. Daarnaast heb je nog applicaties die worden gemaakt met AI, Automatisering en nog veel meer.</p> 
             <br>
             <p>Zoveel keus, maar <span>waar moet jij beginnen?</span></p>
             <br>
